17786f6a6a046ee0475148ba43e43ef26ebe3c49
wendy0705 <wendy9007054@gmail.com>
2024-06-22 17:19:57 +0800

    To number

--- Week-1/Assignment-3/basic.js
+++ Week-1/Assignment-3/basic.js
@@ -10,12 +10,21 @@ function countAandB(input) {
 
 function toNumber(input) { 
     // your code here
-    
+    var output = [];
+    for(let i = 0; i < input.length; i++){
+        if(input[i] === 'a') 
+            output.push(1);
+        else if(input[i] === 'b')
+            output.push(2);
+        else
+            output.push(3);
+    }
+    return output;
 }
 
 let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c']; 
 console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
-// console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
 
 let input2 = ['e', 'd', 'c', 'd', 'e']; 
 console.log(countAandB(input2)); // should print 0 console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
