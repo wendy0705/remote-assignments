44d615a56ae56ac448d00af24023a88bfc26673a
wendy0705 <wendy9007054@gmail.com>
2024-06-24 18:00:53 +0800

    toNumber-revised version

--- Week-1/Assignment-3/basic.js
+++ Week-1/Assignment-3/basic.js
@@ -16,8 +16,12 @@ function toNumber(input) {
             output.push(1);
         else if(input[i] === 'b')
             output.push(2);
-        else
+        else if(input[i] === 'c')
             output.push(3);
+        else if(input[i] === 'd')
+            output.push(4);
+        else if(input[i] === 'e')
+            output.push(5);
     }
     return output;
 }
@@ -27,4 +31,5 @@ console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b
 console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
 
 let input2 = ['e', 'd', 'c', 'd', 'e']; 
-console.log(countAandB(input2)); // should print 0 console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
+console.log(countAandB(input2)); // should print 0 console.log(toNumber(input2)); 
+console.log(toNumber(input2));// should print [5, 4, 3, 4, 5]
