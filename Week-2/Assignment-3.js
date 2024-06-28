711db094ab700a9299f15902515e31b6e00ab1f4
wendy0705 <wendy9007054@gmail.com>
2024-06-28 23:14:56 +0800

    assignment-3 count()

---
+++ Week-2/Assignment-3.js
@@ -0,0 +1,57 @@
+function count(input) {
+    // your code here
+    let counts = [{
+        alp: 'a',
+        count: 0
+    },
+    {
+        alp: 'b',
+        count: 0
+    },
+    {
+        alp: 'c',
+        count: 0
+    },
+    {
+        alp: 'x',
+        count: 0
+    }];
+
+    for(let i = 0; i < input.length; i++){
+        if(input[i] === 'a'){
+            counts[0].count += 1;
+        }else if(input[i] === 'b'){
+            counts[1].count += 1;
+        }else if(input[i] === 'c'){
+            counts[2].count += 1;
+        }else if(input[i] === 'x'){
+            counts[3].count += 1;
+        }
+    }
+
+    const alpcount = counts.reduce((alpcount, num) => {
+        alpcount[num.alp] = num.count;
+        return alpcount;
+    },{});
+
+    return alpcount;
+}
+let input1 = ["a", "b", "c", "a", "c", "a", "x"];
+console.log(count(input1));
+// should print {a:3, b:1, c:2, x:1}
+
+function groupByKey(input) {
+    // your code here
+
+}
+
+let input2 = [
+    { key: "a", value: 3 },
+    { key: "b", value: 1 },
+    { key: "c", value: 2 },
+    { key: "a", value: 3 },
+    { key: "c", value: 5 },
+];
+
+console.log(groupByKey(input2));
+// should print {a:6, b:1, c:7}
