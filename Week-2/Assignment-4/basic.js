4b2f409c0f4eff0b437a656348664598021bca7a
wendy0705 <wendy9007054@gmail.com>
2024-06-29 18:29:26 +0800

    assignment-4 finished

---
+++ Week-2/Assignment-4/basic.js
@@ -0,0 +1,6 @@
+let action = document.querySelector('#action');
+let contents2 = document.querySelector('#contents2');
+
+action.addEventListener('click', () => {
+    contents2.style.display = 'flex';
+});
