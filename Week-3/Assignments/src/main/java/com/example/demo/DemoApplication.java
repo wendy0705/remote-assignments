b7807c7a819daa6aea73f3246efb30d1ae338666
wendy0705 <wendy9007054@gmail.com>
2024-07-02 15:00:30 +0800

    rename

--- Week-3/Assignments/src/main/java/com/example/demo/DemoApplication.java
+++ Week-3/Assignments/src/main/java/com/example/demo/DemoApplication.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController//make this class to serve endpoints
 public class DemoApplication {
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping
-	public String hello(@RequestParam(value = "name", defaultValue = "My Server!") String name) {
-		return String.format("Hello %s!", name);
+
+	@GetMapping//get things from server
+	public String hello(@RequestParam(value = "name", defaultValue = "My Server") String name) {
+		return String.format("Hello, %s!", name);
 	}
 }
