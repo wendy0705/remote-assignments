00d900f63361ed67995d35bd09979acfbdb73b64
wendy0705 <wendy9007054@gmail.com>
2024-07-02 09:25:41 +0800

    Assignment-4, revised version

--- Week-2/Assignment-4/basic.js
+++ Week-2/Assignment-4/basic.js
@@ -1,6 +1,11 @@
 let action = document.querySelector('#action');
 let contents2 = document.querySelector('#contents2');
+let welcome = document.querySelector('h2');
 
 action.addEventListener('click', () => {
     contents2.style.display = 'flex';
+});
+
+welcome.addEventListener('click', () => {//新增Request 1
+    welcome.textContent = "Have a Good Time!";
 });
