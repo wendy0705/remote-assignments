54c3e8e11ccd828cb9c67d107cb33c94e972583e
wendy0705 <wendy9007054@gmail.com>
2024-07-02 09:19:39 +0800

    Assignment-1, revised version

--- Week-2/Assignment-1/arrayfun.js
+++ Week-2/Assignment-1/arrayfun.js
@@ -1,6 +1,6 @@
 function max(numbers) {
     // your code here, for-loop method preferred
-    let max = 0;
+    let max = -Infinity; //從0改成負無限大，可以解決輸入值小於0的情況
     for(let number of numbers){
         if(number > max){
             max = number;
@@ -12,9 +12,9 @@ function max(numbers) {
 function findPosition (numbers, target) {
     // your code here, for-loop method preferred
     let position = -1;
-    for(let number of numbers){
-        if(number == target){
-            position = numbers.indexOf(number);
+    for(let i = 0; i < numbers.length; i++){//將迴圈從let number of numbers改成用index i來跑，便可以直接讀i = target在numbers中的位置
+        if(numbers[i] == target){
+            position = i;
             return position;
         }
     }
