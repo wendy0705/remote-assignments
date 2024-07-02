cabab00f338071c50810b13f7631eb0a08e25446
wendy0705 <wendy9007054@gmail.com>
2024-07-02 10:11:05 +0800

    Assignment-3, revised version

--- Week-2/Assignment-3/data.js
+++ Week-2/Assignment-3/data.js
@@ -1,40 +1,12 @@
 function count(input) {
     // your code here
-    let counts = [{
-        alp: 'a',
-        count: 0
-    },
-    {
-        alp: 'b',
-        count: 0
-    },
-    {
-        alp: 'c',
-        count: 0
-    },
-    {
-        alp: 'x',
-        count: 0
-    }];
-
-    for(let i = 0; i < input.length; i++){
-        if(input[i] === 'a'){
-            counts[0].count += 1;
-        }else if(input[i] === 'b'){
-            counts[1].count += 1;
-        }else if(input[i] === 'c'){
-            counts[2].count += 1;
-        }else if(input[i] === 'x'){
-            counts[3].count += 1;
-        }
-    }
-
-    const alpcount = counts.reduce((alpcount, num) => {
-        alpcount[num.alp] = num.count;
-        return alpcount;
+    let result;
+    result = input.reduce((result, n) => {
+        result[n] = (result[n]||0) + 1;//定義result[n]初始值為０
+        return result;
     },{});
+    return result;
 
-    return alpcount;
 }
 let input1 = ["a", "b", "c", "a", "c", "a", "x"];
 console.log(count(input1));
@@ -42,35 +14,11 @@ console.log(count(input1));
 
 function groupByKey(input) {
     // your code here
-    let output = [{
-        key: 'a',
-        value: 0
-    },
-    {
-        key: 'b',
-        value: 0
-    },
-    {
-        key: 'c',
-        value: 0
-    }];
-
-    for(let i = 0; i < input.length; i++){
-        if(input[i].key === 'a'){
-            output[0].value += input[i].value;
-        }else if(input[i].key === 'b'){
-            output[1].value += input[i].value;
-        }else if(input[i].key === 'c'){
-            output[2].value += input[i].value;
-        }
-    }
-
-    const alpcount = output.reduce((alpcount, num) => {
-        alpcount[num.key] = num.value;
-        return alpcount;
+    let result;
+    result = input.reduce((result, n) => {
+        result[n.key] = (result[n.key]||0) + n.value;
     },{});
-
-    return alpcount;
+    return result;
 }
 
 let input2 = [
