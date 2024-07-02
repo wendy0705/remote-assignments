e483a9424925b2fa26b4e94aa8e86aa13bde23d8
wendy0705 <wendy9007054@gmail.com>
2024-07-02 17:50:27 +0800

    data controller

--- Week-3/Assignments/src/main/java/com/example/demo/data/DataController.java
+++ Week-3/Assignments/src/main/java/com/example/demo/data/DataController.java
@@ -1,16 +1,33 @@
 package com.example.demo.data;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.http.HttpStatus;
 
 @RestController//make this class to serve endpoints
-@RequestMapping(path = "/getData")
-public class DataController {
+@RequestMapping
+public class DataController{
 
-    @GetMapping//get things from server
-    public String hello() {
-        return "Lack of Parameter";
+    @GetMapping(path = "/getData")
+    public String getData(@RequestParam(value = "number", required = false) Integer n) {
+
+        if (n == null) {
+            return "Lack of Parameter";
+        }
+        else {
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+            if(n > 1000363) return "Too Large";
+            return Integer.toString(sum);
+        }
     }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleTypeMismatch() {
+        return "Wrong Parameter";
+    }
+
 }
