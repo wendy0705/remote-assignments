9fde2af165f77d7445f29757015402c90ffc7f7b
wendy0705 <wendy9007054@gmail.com>
2024-07-05 15:34:14 +0800

    Add cookie

--- Week-3/Assignments/src/main/java/com/example/demo/data/DataController.java
+++ Week-3/Assignments/src/main/java/com/example/demo/data/DataController.java
@@ -1,31 +1,37 @@
 package com.example.demo.data;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.http.HttpStatus;
 
 @RestController//make this class to serve endpoints
 @RequestMapping
-public class DataController{
+public class DataController {
 
     @GetMapping(path = "/data")
-    public String getData(@RequestParam(value = "number", required = false) Integer n) {
-
-        if (n == null) {
-            return "Lack of Parameter";
-        } else {
-            int sum = 0;
-            for (int i = 1; i <= n; i++) {
-                sum += i;
+    public String getData(@RequestParam(value = "number", required = false) String n) {//if input is integer, it can turn to string automatically, which won't result in NumberFormatException
+        try {
+            if (n == null || n.isEmpty()) {
+                return "Lack of Parameter";
+            } else {
+                int number = Integer.parseInt(n);
+                int sum = 0;
+                for (int i = 1; i <= number; i++) {
+                    sum += i;
+                }
+                if (number > 1000363)
+                    return "Too Large";//when number > 1000363, 1+2+...+1000363 will larger than max integer which is 2147483647
+                return Integer.toString(sum);
             }
-            if(n > 1000363) return "Too Large";//when n > 1000363, 1+2+...+1000363 will larger than max integer which is 2147483647
-            return Integer.toString(sum);
+        } catch (NumberFormatException e) {
+            return "Wrong Parameter";
         }
     }
-
-    @ExceptionHandler(RuntimeException.class)
-    public String handleTypeMismatch() {
-        return "Wrong Parameter";
-    }
+//    @GetMapping(path = "/myName")
+//    public String myname(String n) {
+//
+//        return n;
+//    }
 
 }
---
+++ Week-3/Assignments/src/main/java/com/example/demo/user/UserController.java
@@ -0,0 +1,41 @@
+package com.example.demo.user;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.http.ResponseCookie;
+import org.springframework.web.bind.annotation.*;
+
+@Controller//make this class to serve endpoints
+@RequestMapping
+public class UserController {
+
+    @GetMapping(value = "/myName")
+//    @ResponseBody
+    public String checkCookie(@CookieValue(value = "user-id", required = false) String userId, Model model) {
+        if (userId != null) {
+            model.addAttribute("userId", userId);
+            return "name";
+        }else {
+            return "welcome";//null, ask user name
+        }
+    }
+
+    @PostMapping("/trackName")
+    public ResponseEntity<Void> setCookie(@RequestParam(value = "name", required = false) String name, HttpServletResponse response) {
+        ResponseCookie springCookie = ResponseCookie.from("user-id", name)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(5)
+                .domain("localhost")
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, springCookie.toString());
+
+        return ResponseEntity.status(302).header(HttpHeaders.LOCATION, "/myName").build();
+    }
+
+}
